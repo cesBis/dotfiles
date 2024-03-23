3a5232e1e6e0703826be5071a1b6d86fbfd9d4dd
Conner Smith <conner.smith@milliman.com>
2024-03-23 07:36:19 -0900

    fix typo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@
 -- example: https://github.com/numToStr/dotfiles/tree/master/neovim/.config/nvim
 -- options: https://neovim.io/doc/user/options.html
 -- lua api: https://neovim.io/doc/user/lua.html
--- installed via `sudo curl -Lo /usr/local/nvim https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && sudo chmod 755 /usr/local/nvim`
+-- installed via `sudo curl -Lo /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && sudo chmod 755 /usr/local/bin/nvim`
 
 require('general')
 require('keys')
