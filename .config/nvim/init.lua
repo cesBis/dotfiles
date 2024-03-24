62d955755f7158e3f8a320e7c4f92c9a392a435e
Conner Smith <conner.smith@milliman.com>
2024-03-24 17:27:18 -0400

    bring on more IDE features

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,15 +2,12 @@
 -- example: https://github.com/numToStr/dotfiles/tree/master/neovim/.config/nvim
 -- options: https://neovim.io/doc/user/options.html
 -- lua api: https://neovim.io/doc/user/lua.html
--- installed via `sudo curl -Lo /usr/local/nvim https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && sudo chmod 755 /usr/local/nvim`
+-- installed via `sudo curl -Lo /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && sudo chmod 755 /usr/local/bin/nvim`
 
 require('general')
 require('keys')
 require('macros')
 require('plugins')
-
--- for clipboard, using win32yank from windows neovim installation seems to work
---   `ln -s /mnt/c/Users/Conner.Smith/programs/nvim-win64/bin/win32yank.exe /usr/local/bin/win32yank.exe`
 
 -- plugin specific options can go in plugins/<name>.lua
 -- see http://neovim.io/doc/user/options.html#'runtimepath'
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -17,7 +17,11 @@ vim.opt.rtp:prepend(lazypath)
 -- https://github.com/folke/lazy.nvim#-plugin-spec
 require("lazy").setup({
     {'karoliskoncevicius/vim-sendtowindow'},
+    {'francoiscabrol/ranger.vim', dependencies = {'rbgrouleff/bclose.vim'}},
     {'ap/vim-css-color'},
     {'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup() end},
-    {'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons'}}
+    {'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons'}},
+    {'neoclide/coc.nvim', branch = 'release'}
 })
+-- https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions#install-extensions
+vim.g.coc_global_extensions = {'coc-json', 'coc-r-lsp', 'coc-jedi'}
