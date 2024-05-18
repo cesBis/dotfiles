e436ff7df3cc185c0a92754c4ba6ee083bb28ba2
Conner Smith <conner.smith@milliman.com>
2024-05-18 14:20:53 -0400

    consolidate nvim keys into keys.lua
    rather than having some in plugin specific configuration

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,15 +1,10 @@
-vim.g.mapleader = ' '
+-------------------------------------
+-- "Pure" NeoVim Keys
+-------------------------------------
 
+vim.g.mapleader = ' '
 local keyset = vim.keymap.set
 local opts = {silent = true}
-
--- start terminal
-keyset('n', '<leader>t', '<cmd>ReplToggle<cr>', opts)
--- exit terminal with Ctrl+q
-keyset('t', '<C-q>', '<C-\\><C-n>')
--- send line to terminal. uses vim-sendtowindow and assumes terminal is below
-keyset('n', '<leader>r', '<Plug>ReplSendLine', opts)
-keyset('x', '<leader>r', '<Plug>ReplSendVisual', opts)
 
 -- tab shortcuts
 keyset('n', '<leader>c', ':tabnew<cr>', opts)
@@ -32,17 +27,40 @@ keyset('n', '<leader>l', ':nohl<cr>:echo<cr><Esc>', opts)
 keyset('n','<leader>w', ':set wrap<cr>', opts)
 keyset('n','<leader>uw', ':set nowrap<cr>', opts)
 
--- fzf from apt
+-------------------------------------
+-- Plugin Keys
+-------------------------------------
+
+-- nvim-repl ------------------------
+keyset('n', '<leader>t', '<cmd>ReplToggle<cr>', opts)
+keyset('n', '<leader>r', '<Plug>ReplSendLine', opts)
+keyset('x', '<leader>r', '<Plug>ReplSendVisual', opts)
+-- exit terminal with Ctrl+q
+keyset('t', '<C-q>', '<C-\\><C-n>')
+
+-- fzf ------------------------------
 keyset('n', '<leader>f', ':FZF<cr>', opts)
 
--- github copilot
+-- github copilot -------------------
 -- from :help copilot
 -- accept on `->` arrow key, like my shell autocomplete, not TAB, that's for coc
 keyset('i', '<Right>', 'copilot#Accept("\\<CR>")', {expr = true,  replace_keycodes = false})
 keyset('i', '<C-Right>', '<Plug>(copilot-accept-word)')
 vim.g.copilot_no_tab_map = true
 
--- keys brought on by plugins
+-- CoC ------------------------------
+-- see `:h coc-list` and search 'coc-list-link' or 'coc-list-diagnostics'
+keyset("n", "Co", ":<C-u>CocList outline<cr>", {silent = true, nowait = true})
+-- see `:h coc-lsp`
+-- Use `[g` and `]g` to navigate diagnostics
+-- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
+keyset("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
+keyset("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
+-- GoTo code navigation.
+keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+
+-- keys brought on by plugins --------
 --
 -- gc for visual mode and motion comments, <int>gcc for commenting int lines from vim-commentary
 -- cs: change surrounding | ds: delete surrounding | ys: yes new surrounding | and more! see https://github.com/tpope/vim-surround
+-- TAB from ../plugin/coc.lua
