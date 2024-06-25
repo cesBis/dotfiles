dcb28306bac42b971fb60c3e38435536b4af70a4
Conner Smith <conner.smith@milliman.com>
2024-06-24 23:30:42 -0400

    update comments in .config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,7 @@
 -- neovim configuration
--- example: https://github.com/numToStr/dotfiles/tree/master/neovim/.config/nvim
 -- options: https://neovim.io/doc/user/options.html
 -- lua api: https://neovim.io/doc/user/lua.html
--- installed via `sudo curl -Lo /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage && sudo chmod 755 /usr/local/bin/nvim`
+-- installed in codespace setup script by curling the appimage
 
 require('general')
 require('keys')
