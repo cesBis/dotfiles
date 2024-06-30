b0ff6bd50be129c5f1c9e6882b8587ef60f264ff
Conner Smith <conner.smith@milliman.com>
2024-06-29 21:26:41 -0400

    nvim repl always open zsh

--- .config/nvim/plugin/nvim-repl.lua
+++ .config/nvim/plugin/nvim-repl.lua
@@ -1,1 +1,5 @@
 vim.g.repl_split = "bottom"
+
+-- for now I always want zsh, so I can elect R versions and whatnot
+-- it's possible to have specifc files start specific interpreters. see https://github.com/pappasam/nvim-repl
+vim.g["repl_filetype_commands"] = {"zsh"}
