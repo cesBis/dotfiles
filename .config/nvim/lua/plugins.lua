86d667d457e43e86604dbd80f23e792020b3338b
Conner Smith <conner.smith@milliman.com>
2024-07-02 23:09:17 -0400

    use apt install fzf.vim for nvim instead of duplicating with lazy

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -17,7 +17,6 @@ vim.opt.rtp:prepend(lazypath)
 -- https://github.com/folke/lazy.nvim#-plugin-spec
 require("lazy").setup({
     {"pappasam/nvim-repl"},
-    {'junegunn/fzf', tag = '0.44.1'},-- fzf from apt
     {'ap/vim-css-color'},
     {'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup() end},
     {'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons'}},
@@ -30,3 +29,6 @@ require("lazy").setup({
 -- https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions#install-extensions
 vim.g.coc_global_extensions = {'coc-json', 'coc-r-lsp', 'coc-jedi'}
 -- see more language servers at https://github.com/neoclide/coc.nvim/wiki/Language-servers
+
+-- fzf installed via apt
+vim.cmd.source("/usr/share/doc/fzf/examples/fzf.vim")
