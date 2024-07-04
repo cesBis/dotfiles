1842abafaf2c5995a14510e12cb66524fed2cfb1
Conner Smith <conner.smith@milliman.com>
2024-07-04 12:39:20 -0400

    refactor dryness .config/nvim/plugin/fzf.lua

--- .config/nvim/plugin/fzf.lua
+++ .config/nvim/plugin/fzf.lua
@@ -1,26 +1,27 @@
 -- https://github.com/junegunn/fzf/blob/master/README-VIM.md
 
-fzf_spec_default = {
-    options = "--preview 'cat {}'",
-    sink = "e",
-}
+local fzf = function(spec) vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec)) end
 
 fzf_git_ls_files = function()
-    local spec = fzf_spec_default
-    spec["source"] = "git ls-files"
-    vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec))
+    fzf({
+        source = "git ls-files",
+        options = "--preview 'cat {}'",
+        sink = "e",
+    })
 end
 
 fzf_all_files = function()
-    local spec = fzf_spec_default
-    spec["source"] = "find . -type f | cut -c 3- | sort"
-    vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec))
+    fzf({
+        source = "find . -type f | cut -c 3- | sort",
+        options = "--preview 'cat {}'",
+        sink = "e",
+    })
 end
 
 fzf_find_in_git_files = function(term)
-    vim.fn["fzf#run"](vim.fn["fzf#wrap"]({
+    fzf({
         sink = "e",
         source = "git grep " .. term .. " | cut -d : -f 1 | sort | uniq",
         options = "--preview 'grep --line-number \"" .. term .. "\" {}'",
-    }))
+    })
 end
