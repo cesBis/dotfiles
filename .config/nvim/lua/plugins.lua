28a919b6fb788de6e8a5de4bd8ffd69788888431
Conner Smith <conner.smith@milliman.com>
2024-07-04 12:55:53 -0400

    2 space tabs for everything but python. apply rule to lua config files.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,15 +16,15 @@ vim.opt.rtp:prepend(lazypath)
 
 -- https://github.com/folke/lazy.nvim#-plugin-spec
 require("lazy").setup({
-    {"pappasam/nvim-repl"},
-    {'ap/vim-css-color'},
-    {'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup() end},
-    {'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons'}},
-    {'tpope/vim-fugitive'},
-    {'tpope/vim-commentary'},
-    {'tpope/vim-surround'},
-    {'github/copilot.vim'},
-    {'neoclide/coc.nvim', branch = 'release'}
+  {"pappasam/nvim-repl"},
+  {'ap/vim-css-color'},
+  {'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup() end},
+  {'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons'}},
+  {'tpope/vim-fugitive'},
+  {'tpope/vim-commentary'},
+  {'tpope/vim-surround'},
+  {'github/copilot.vim'},
+  {'neoclide/coc.nvim', branch = 'release'}
 })
 -- https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions#install-extensions
 vim.g.coc_global_extensions = {'coc-json', 'coc-r-lsp', 'coc-jedi'}
--- .config/nvim/plugin/fzf.lua
+++ .config/nvim/plugin/fzf.lua
@@ -3,25 +3,25 @@
 local fzf = function(spec) vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec)) end
 
 fzf_git_ls_files = function()
-    fzf({
-        source = "git ls-files",
-        options = "--preview 'cat {}'",
-        sink = "e",
-    })
+  fzf({
+    source = "git ls-files",
+    options = "--preview 'cat {}'",
+    sink = "e",
+  })
 end
 
 fzf_all_files = function()
-    fzf({
-        source = "find . -type f | cut -c 3- | sort",
-        options = "--preview 'cat {}'",
-        sink = "e",
-    })
+  fzf({
+    source = "find . -type f | cut -c 3- | sort",
+    options = "--preview 'cat {}'",
+    sink = "e",
+  })
 end
 
 fzf_find_in_git_files = function(term)
-    fzf({
-        sink = "e",
-        source = "git grep " .. term .. " | cut -d : -f 1 | sort | uniq",
-        options = "--preview 'grep --line-number \"" .. term .. "\" {}'",
-    })
+  fzf({
+    sink = "e",
+    source = "git grep " .. term .. " | cut -d : -f 1 | sort | uniq",
+    options = "--preview 'grep --line-number \"" .. term .. "\" {}'",
+  })
 end
