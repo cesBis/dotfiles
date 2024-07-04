e8a54ccf40d361cbb8c6b31f933ef25b03c5e40c
Conner Smith <conner.smith@milliman.com>
2024-07-04 12:19:17 -0400

    implement fzf_find_in_git_files on fzf.vim

--- .config/nvim/plugin/fzf.lua
+++ .config/nvim/plugin/fzf.lua
@@ -1,6 +1,5 @@
--- used to keyset fzf with different search lists
+-- https://github.com/junegunn/fzf/blob/master/README-VIM.md
 
--- https://github.com/junegunn/fzf/blob/master/README-VIM.md#fzfwrap
 fzf_spec_default = {
     options = "--preview 'cat {}'",
     sink = "e",
@@ -16,4 +15,12 @@ fzf_all_files = function()
     local spec = fzf_spec_default
     spec["source"] = "find . -type f | cut -c 3- | sort"
     vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec))
+end
+
+fzf_find_in_git_files = function(term)
+    vim.fn["fzf#run"](vim.fn["fzf#wrap"]({
+        sink = "e",
+        source = "git grep " .. term .. " | cut -d : -f 1 | sort | uniq",
+        options = "--preview 'grep --line-number \"" .. term .. "\" {}'",
+    }))
 end
