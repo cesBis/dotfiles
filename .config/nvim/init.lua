73fbdf5bde05c198561fc40133d7c5926652b42a
Conner Smith <conner.smith@milliman.com>
2024-07-12 00:42:07 -0400

    organize and comment nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,16 @@
--- installed via apt
--- options: https://neovim.io/doc/user/options.html
--- lua api: https://neovim.io/doc/user/lua.html
+---------------------------------------------------------------------------------------------------------
+-- https://neovim.io/doc/user/options.html
+-- https://neovim.io/doc/user/lua.html
 -- :h runtimepath
+-- installed via apt
 --
 -- for clipboard, using win32yank from windows neovim installation seems to work
 --   `ln -s /mnt/c/Users/Conner.Smith/programs/nvim-win64/bin/win32yank.exe /usr/local/bin/win32yank.exe`
+---------------------------------------------------------------------------------------------------------
 
-require('macros')
+-------------------------------------
+-- lazy.nvim
+-------------------------------------
 
 -- https://github.com/folke/lazy.nvim#-plugin-spec
 require('lazy-init')
@@ -22,13 +26,23 @@ require("lazy").setup({
   {'neoclide/coc.nvim', branch = 'release'}
 })
 
+-------------------------------------
+-- commands (some plugin dependence)
+-------------------------------------
+
+require('macros')
+require('keys')
+
+-------------------------------------
+-- miscellaneous
+-------------------------------------
+
+vim.cmd.set('splitbelow splitright number relativenumber nowrap mouse=a encoding=utf8 clipboard=unnamedplus,unnamed ignorecase smartcase')
+vim.cmd.colorscheme('mine')
+
 -- tab characters and spaces: first and last line set default, middle lines set exceptions
 vim.cmd [[
   set expandtab tabstop=2 shiftwidth=2
   autocmd BufEnter *.py set tabstop=4|set shiftwidth=4
   autocmd BufLeave * set tabstop=2|set shiftwidth=2
 ]]
-
-vim.cmd.colorscheme('mine')
-vim.cmd.set('splitbelow splitright number relativenumber nowrap mouse=a encoding=utf8 clipboard=unnamedplus,unnamed ignorecase smartcase')
-require('keys')
