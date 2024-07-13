1695f8d2d3899efcf0d9d7c314f982015da078df
Conner Smith <conner.smith@milliman.com>
2024-07-13 14:33:11 -0400

    modify nvim lualine

--- .config/nvim/plugin/lualine.lua
+++ .config/nvim/plugin/lualine.lua
@@ -1,25 +1,16 @@
 -- https://github.com/nvim-lualine/lualine.nvim#default-configuration
+-- only need to be explicit when varying from default
 
 require('lualine').setup {
   options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    always_divide_middle = true,
     globalstatus = true,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {{'filename', path = 1}},
-    lualine_x = {'encoding', 'fileformat'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  }
+    lualine_a = {{'filename', path = 1}},
+    lualine_b = {'progress'},
+    lualine_c = {'diagnostics'},
+    lualine_x = {},
+    lualine_y = {'diff', 'branch'},
+    lualine_z = {'filetype', 'encoding', 'fileformat'},
+  },
 }
