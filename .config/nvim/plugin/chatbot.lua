bc42412b0df3a943684df2861d9d4b0397524470
Conner Smith <conner.smith@milliman.com>
2024-08-17 14:31:19 -0400

    create DocBot agent in nvim/plugin/chatbot.lua

--- .config/nvim/plugin/chatbot.lua
+++ .config/nvim/plugin/chatbot.lua
@@ -4,12 +4,29 @@ require("gp").setup({
     copilot = {disabled = false},
     openai = {disabled = true},
   },
+  agents = {
+    -- a ChatCopilot agent is provided by default
+    -- change agents with :GpAgent ChatCopilot
+    -- agent choice is persisted on disk across sessions
+    {
+      provider = "copilot",
+      name = "DocBot",
+      chat = true,
+      command = false,
+      model = { model = "gpt-4o", temperature = 0.3, top_p = 1 },
+      system_prompt = "You are a customized internet search engine for a computer programmer.\n\n"
+      .. "The user provided a search phrase:\n\n"
+      .. "- Respond only with a markdown formatted list of valid links.\n"
+      .. "- Prioritize official documentation.\n"
+    }
+  },
   chat_template = require("gp.defaults").short_chat_template,
+  -- disabling shortcuts in favor of custom commands below
   chat_shortcut_respond = {modes = {}, shortcut = ''},
   chat_shortcut_delete = {modes = {}, shortcut = ''},
   chat_shortcut_stop = {modes = {}, shortcut = ''},
   chat_shortcut_new = {modes = {}, shortcut = ''},
 })
 
-vim.api.nvim_create_user_command("Chatstart", ":GpChatNew tabnew", {})
-vim.api.nvim_create_user_command("Chatreponse", ":GpChatRespond", {})
+vim.api.nvim_create_user_command("Botstart", ":GpChatNew tabnew", {})
+vim.api.nvim_create_user_command("Botresponse", ":GpChatRespond", {})
