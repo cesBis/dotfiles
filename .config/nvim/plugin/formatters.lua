c29759ba9c372b22e31c5428dd2034c480b56305
Conner Smith <smith_ces@outlook.com>
2024-09-17 20:11:10 -0400

    start nvim/plugin/formatters.lua with ruff

---
+++ .config/nvim/plugin/formatters.lua
@@ -0,0 +1,13 @@
+vim.fn.system("which ruff")
+if vim.v.shell_error == 0 then
+  vim.api.nvim_create_autocmd(
+    "BufWritePost",
+    {
+      pattern = "*.py",
+      callback = function()
+        os.execute("ruff format --quiet")
+        vim.cmd.edit(vim.fn.expand("%"))
+      end
+    }
+  )
+end
