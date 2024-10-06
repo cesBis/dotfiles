f3d5b3c2f083ef5d7fbe2fed5d67660d2aff66ba
Conner Smith <smith_ces@outlook.com>
2024-10-05 21:20:34 -0400

    add nvim command Help

--- .config/nvim/plugin/macros.lua
+++ .config/nvim/plugin/macros.lua
@@ -5,6 +5,7 @@ local macro = vim.api.nvim_create_user_command
 
 macro("Spellcheck", ":setlocal spell spelllang=en_us", {})
 macro("Nospellcheck", ":setlocal nospell", {})
+macro("Help", ":tab help", {nargs=1})
 
 -- :Git from vim-fugitive
 macro("Commit", ":Git add --update | tab Git commit --verbose", {})
