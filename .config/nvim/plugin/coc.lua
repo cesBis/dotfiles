24fb45a7541bb074f3830d129107910ce4a16bc7
Conner Smith <conner.smith@milliman.com>
2024-11-12 18:19:54 -0500

    cleanup nvim coc.lua

--- .config/nvim/plugin/coc.lua
+++ .config/nvim/plugin/coc.lua
@@ -6,38 +6,10 @@ vim.g.coc_global_extensions = {'coc-json', 'coc-r-lsp', 'coc-jedi', '@yaegassy/c
 -- Some servers have issues with backup files, see #649.
 vim.opt.backup = false
 vim.opt.writebackup = false
-
--- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
--- delays and poor user experience.
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience.
 vim.opt.updatetime = 300
-
--- Always show the signcolumn, otherwise it would shift the text each time
--- diagnostics appear/become resolved.
+-- Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved.
 vim.opt.signcolumn = "yes"
-
--- Highlight the symbol and its references when holding the cursor.
-vim.api.nvim_create_augroup("CocGroup", {})
-vim.api.nvim_create_autocmd("CursorHold", {
-    group = "CocGroup",
-    command = "silent call CocActionAsync('highlight')",
-    desc = "Highlight symbol under cursor on CursorHold"
-})
-
--- Setup formatexpr specified filetype(s).
-vim.api.nvim_create_autocmd("FileType", {
-    group = "CocGroup",
-    pattern = "typescript,json",
-    command = "setl formatexpr=CocAction('formatSelected')",
-    desc = "Setup formatexpr specified filetype(s)."
-})
-
--- Update signature help on jump placeholder.
-vim.api.nvim_create_autocmd("User", {
-    group = "CocGroup",
-    pattern = "CocJumpPlaceholder",
-    command = "call CocActionAsync('showSignatureHelp')",
-    desc = "Update signature help on jump placeholder"
-})
 
 -- Auto complete helper function
 function _G.check_back_space()
