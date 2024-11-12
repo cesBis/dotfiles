4945978c8c6bbba3007681272f618dfe0e96de39
Conner Smith <conner.smith@milliman.com>
2024-11-12 18:24:38 -0500

    cleanup comments

--- .config/nvim/plugin/coc.lua
+++ .config/nvim/plugin/coc.lua
@@ -1,9 +1,7 @@
--- https://github.com/neoclide/coc.nvim#example-lua-configuration
--- https://github.com/neoclide/coc.nvim/wiki/Using-coc-extensions#install-extensions
 -- see more language servers at https://github.com/neoclide/coc.nvim/wiki/Language-servers
 vim.g.coc_global_extensions = {'coc-json', 'coc-r-lsp', 'coc-jedi', '@yaegassy/coc-marksman'}
 
--- Some servers have issues with backup files, see #649.
+-- Some servers have issues with backup files, https://github.com/neoclide/coc.nvim/issues/649#issuecomment-480086894
 vim.opt.backup = false
 vim.opt.writebackup = false
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience.
