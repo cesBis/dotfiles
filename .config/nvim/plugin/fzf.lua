9a3bf2bbd5b4c2a3209b84790cbe4f6c04e1f17d
Conner Smith <conner.smith@milliman.com>
2024-11-15 12:48:44 -0500

    ignore some directories in nvim fzf tools

--- .config/nvim/plugin/fzf.lua
+++ .config/nvim/plugin/fzf.lua
@@ -6,7 +6,12 @@ local fzf = function(spec) vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec)) end
 
 fzf_files = function()
   fzf({
-    source = "find . -path './.git' -prune -o -type f -print | cut -c 3- | sort",
+    source = "find . "
+      .. "-path './.git' -prune -o "
+      .. "-path './.venv' -prune -o "
+      .. "-path '*__pycache__' -prune -o "
+      .. "-path './renv/library' -prune -o "
+      .. "-type f -print | cut -c 3- | sort",
     options = "--preview 'cat {}'",
     sink = "e",
   })
@@ -14,7 +19,12 @@ end
 
 fzf_dirs = function()
   fzf({
-    source = "find . -path './.git' -prune -o -type d -print | tail +2 | cut -c 3- | sort",
+    source = "find . "
+      .. "-path './.git' -prune -o "
+      .. "-path './.venv' -prune -o "
+      .. "-path '*__pycache__' -prune -o "
+      .. "-path './renv/library' -prune -o "
+      .. "-type d -print | tail +2 | cut -c 3- | sort",
     options = "--preview 'ls -A {}'",
     sink = "e",
   })
