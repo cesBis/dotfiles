ed5e55350c4b772c23d355b3b6b7b42a4cbe9b57
Conner Smith <conner.smith@milliman.com>
2024-12-05 18:29:27 -0500

    always start repl with zsh

--- .config/nvim/plugin/nvim-repl.lua
+++ .config/nvim/plugin/nvim-repl.lua
@@ -1,7 +1,3 @@
 vim.g.repl_split = "bottom"
-
-vim.g["repl_filetype_commands"] = {
-  R = "R --quiet",
-  rmd = "R --quiet",
-  python = "python"
-}
+-- it's possible to have specific files start specific interpreters. see https://github.com/pappasam/nvim-repl
+vim.g.repl_filetype_commands = {"zsh"}
