f9459ca10a284e9582f522efc5a2ebe7b3ee0832
Conner Smith <conner.smith@milliman.com>
2024-12-11 17:45:21 -0500

    remove unnecessary vim.cmd.source from nvim/plugin/fzf.lua
    nix handles getting it on the nvim runtimepath for me

--- .config/nvim/plugin/fzf.lua
+++ .config/nvim/plugin/fzf.lua
@@ -1,6 +1,4 @@
 -- https://github.com/junegunn/fzf/blob/master/README-VIM.md
-vim.cmd.source("~/.nix-profile/share/vim-plugins/fzf/plugin/fzf.vim")
-
 local fzf = function(spec) vim.fn["fzf#run"](vim.fn["fzf#wrap"](spec)) end
 
 fzf_files = function()
