731533ab71ecaef4302ca402219beb24d354c750
Conner Smith <conner.smith@milliman.com>
2025-04-15 16:06:47 -0400

    add air R formatter to nvim/plugin/formatters and use vim builtin for detecting if formatter is present

--- .config/nvim/plugin/formatters.lua
+++ .config/nvim/plugin/formatters.lua
@@ -1,11 +1,23 @@
-vim.fn.system("which ruff")
-if vim.v.shell_error == 0 then
+if vim.fn.executable("ruff") == 1 then
   vim.api.nvim_create_autocmd(
     "BufWritePost",
     {
       pattern = "*.py",
       callback = function()
-        os.execute("ruff format --quiet " .. vim.fn.expand("%"))
+        vim.fn.system("ruff format --quiet " .. vim.fn.expand("%"))
+        vim.cmd.edit(vim.fn.expand("%"))
+      end
+    }
+  )
+end
+
+if vim.fn.executable("air") == 1 then
+  vim.api.nvim_create_autocmd(
+    "BufWritePost",
+    {
+      pattern = "*.R",
+      callback = function()
+        vim.fn.system("air format --quiet " .. vim.fn.expand("%"))
         vim.cmd.edit(vim.fn.expand("%"))
       end
     }
