f5be0693a1093fb24ff9d03c8b7d1ab5af037206
Conner Smith <conner.smith@milliman.com>
2025-10-21 15:41:37 -0400

    add more copilot model options for gp.nvim
    to be able to quickly switch between them and try them out

--- .config/nvim/lua/gp_conf.lua
+++ .config/nvim/lua/gp_conf.lua
@@ -1,5 +1,14 @@
 if os.getenv('OLLAMA_HOST') then ollama_host = os.getenv('OLLAMA_HOST') else ollama_host = '' end
 
+local prompt = "You are an experienced computer programmer."
+            .." An excellent mentor and helpful assistant, but terse, blunt, and focused."
+            .." An expert in related documentation, including docs.python.org, and the unix `man` pages."
+            .." Please help me find related documentation."
+            .." Help by providing reference examples, and discussing ideas."
+            .." Be concise."
+            .." Don't provide advice."
+            .." Use examples to introduce concepts and **keywords**."
+
 -- https://github.com/Robitx/gp.nvim
 return {
 
@@ -23,35 +32,38 @@ return {
     -- agent choice is persisted on disk across sessions
     {
       provider = "copilot",
-      name = "CodeDuckCopilot",
+      name = "CustomCopilotClaude",
       chat = true,
       command = false,
-      -- "gpt-5" and "gpt-5-mini" are also tested working values. gpt-5-mini doesn't consume premium requests, and seems faster
       -- see premium request usage at https://github.com/settings/copilot/features
       model = { model = "claude-sonnet-4.5" },
-      system_prompt = "You are an experienced computer programmer."
-                   .." An excellent mentor and helpful assistant, but terse, blunt, and focused."
-                   .." An expert in related documentation, including docs.python.org, and the unix `man` pages."
-                   .." Please help me find related documentation."
-                   .." Help by providing reference examples, and discussing ideas."
-                   .." Be concise."
-                   .." Don't provide advice."
-                   .." Use examples to introduce concepts and **keywords**."
+      system_prompt = prompt
+    },
+    {
+      provider = "copilot",
+      name = "CustomCopilotGpt5",
+      chat = true,
+      command = false,
+      -- see premium request usage at https://github.com/settings/copilot/features
+      model = { model = "gpt-5" },
+      system_prompt = prompt
+    },
+    {
+      provider = "copilot",
+      name = "CustomCopilotGpt5mini",
+      chat = true,
+      command = false,
+      -- this one doesn't use premium requests https://docs.github.com/en/copilot/concepts/billing/copilot-requests#model-multipliers
+      model = { model = "gpt-5-mini" },
+      system_prompt = prompt
     },
     {
       provider = "ollama",
-      name = "CodeDuckLlama",
+      name = "ChatMyLlama",
       chat = true,
       command = false,
       model = { model = "gemma2", temperature = 0.4, top_p = 1 },
-      system_prompt = "You are an experienced computer programmer."
-                   .." An excellent mentor and helpful assistant, but terse, blunt, and focused."
-                   .." An expert in related documentation, including docs.python.org, and the unix `man` pages."
-                   .." Please help me find related documentation."
-                   .." Help by providing reference examples, and discussing ideas."
-                   .." Be concise."
-                   .." Don't provide advice."
-                   .." Use examples to introduce concepts and **keywords**."
+      system_prompt = prompt
     },
   }
 }
