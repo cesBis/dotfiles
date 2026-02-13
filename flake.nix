5ab492bd7c337289f4a645b992a1ef367ac4f783
Conner Smith <smith_ces@outlook.com>
2026-02-13 00:57:53 -0500

    remove rig from flake and add an R 4.3.3 package element

--- flake.nix
+++ flake.nix
@@ -3,14 +3,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     repl = { url = "github:pappasam/nvim-repl"; flake = false; };
     chatbot = { url = "github:robitx/gp.nvim"; flake = false; };
+    # https://history.nix-packages.com/ or github:jamesbrink/nxv
+    R433nixpkgs.url = "github:nixos/nixpkgs/87f7f76";
   };
 
   outputs = inputs @ { self, nixpkgs, ... }: {
 
+    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+
     packages.x86_64-linux.default = self.pkgs.buildEnv {
       name = "devtools";
       paths = with self.pkgs; [
-# https://search.nixos.org/packages
+        # https://search.nixos.org/packages
         zsh zsh-autosuggestions zsh-syntax-highlighting
         tmux bat fzf jq
         self.neovim copilot-language-server
@@ -24,14 +28,10 @@
       ];
     };
 
-    packages.x86_64-linux.R = self.pkgs.buildEnv {
-      name = "R tools";
-      paths = with self.pkgs; [
-        self.rig air-formatter
-      ];
+    packages.x86_64-linux.R433 = self.pkgs.buildEnv {
+      name = "R 4.3.3";
+      paths = [ inputs.R433nixpkgs.legacyPackages.x86_64-linux.R self.pkgs.air-formatter ];
     };
-
-    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
 
     neovim = self.pkgs.wrapNeovimUnstable self.pkgs.neovim-unwrapped (
       self.pkgs.neovimUtils.makeNeovimConfig {
@@ -59,26 +59,5 @@
 
     rollVimPkg = src: pname: self.pkgs.vimUtils.buildVimPlugin { inherit pname src; version = src.lastModifiedDate; };
 
-    rig = self.pkgs.stdenv.mkDerivation {
-      pname = "rig";
-      version = "0.7.1";
-
-      src = self.pkgs.fetchurl {
-        url = "https://github.com/r-lib/rig/releases/download/v0.7.1/rig-linux-0.7.1.tar.gz";
-        sha256 = "5b48c32120d724b3932e1704e5a2d29b6757b6ccea6634876094b59c00075e9e";
-      };
-
-      buildPhase = ''
-        tar --directory . --file $src --extract --auto-compress
-      '';
-
-      sourceRoot = "."; # https://stackoverflow.com/questions/77160863/nix-derivation-error-unpacker-produced-multiple-directories
-
-      installPhase = ''
-        mkdir -p $out/bin
-        cp bin/rig $out/bin/rig
-        cp -r share $out
-      '';
-    };
   };
 }
