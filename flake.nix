08de97667451f082feccb854ec348835034bd1d1
conner <conner.smith@milliman.com>
2026-04-21 17:42:06 -0400

    setup flake apps for different R versions

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     repl = { url = "github:pappasam/nvim-repl"; flake = false; };
     # https://history.nix-packages.com/ or github:jamesbrink/nxv
-    R433nixpkgs.url = "github:nixos/nixpkgs/87f7f76";
+    R433.url = "github:nixos/nixpkgs/87f7f76";
+    R452.url = "github:nixos/nixpkgs/9bce378";
   };
 
   outputs = inputs @ { self, ... }: {
@@ -37,10 +38,9 @@
       ];
     };
 
-    packages.x86_64-linux.R433 = self.pkgs.buildEnv {
-      name = "R 4.3.3";
-      paths = [ inputs.R433nixpkgs.legacyPackages.x86_64-linux.R self.pkgs.air-formatter ];
-    };
+    apps.x86_64-linux.air = { type = "app"; program = "${self.pkgs.air-formatter}/bin/air"; };
+    apps.x86_64-linux.R433 = { type = "app"; program = "${inputs.R433.legacyPackages.x86_64-linux.R}/bin/R"; };
+    apps.x86_64-linux.R452 = { type = "app"; program = "${inputs.R452.legacyPackages.x86_64-linux.R}/bin/R"; };
 
     neovim = self.pkgs.wrapNeovimUnstable self.pkgs.neovim-unwrapped (
       self.pkgs.neovimUtils.makeNeovimConfig {
